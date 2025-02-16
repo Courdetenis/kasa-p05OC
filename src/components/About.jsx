81b03f482d745d4a4563a9e90153190128637592
Alexis 'Tchoupi' Lorant <courdetenis@protonmail.ch>
2025-02-16 17:20:33 +0100

    --- Update --- Ajout de la favicon correspondant au site internet.
    Mise à jour du background image dans la partie about.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,13 +7,30 @@ function About() {
     <>
       <main>
         <div className="banner-about">
-          <img src="https://picsum.photos/800/400" alt="Image test" />
+          <img
+            src="./banner-background-about.jpeg"
+            alt="Image Bannière à propos"
+          />
         </div>
         <div className="about">
-          <Collapse title="Test" content="Test Pour voir" />
-          <Collapse title="Test" content="Test Pour voir" />
-          <Collapse title="Test" content="Test Pour voir" />
-          <Collapse title="Test" content="Test Pour voir" />
+          <Collapse
+            title="Fiabilité"
+            content="Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes."
+          />
+          <Collapse
+            title="Respect"
+            content="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme."
+          />
+          <Collapse
+            title="Service"
+            content="Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question.
+"
+          />
+          <Collapse
+            title="Sécurtié"
+            content="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.
+"
+          />
         </div>
       </main>
     </>
